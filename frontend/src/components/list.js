057209b45d7b02a1c1144dae26cdeaf0c3f04c6e
Otto Kujala <otto.kujala@tuni.fi>
2022-01-13 18:07:57 +0200

    User can now select what tag to use

---
+++ frontend/src/components/list.js
@@ -0,0 +1,85 @@
+import React from "react";
+import Word from "../components/word";
+const axios = require("axios").default;
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: [],
+      check: false,
+      tag: this.props.tag,
+      points: [],
+    };
+
+    this.handler = this.handler.bind(this);
+  }
+
+  handler(id) {
+    let tmp = this.state.points;
+    tmp[id] = 1;
+    this.setState({
+      points: tmp,
+    });
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "http://localhost:8080/words/tag/" + this.state.tag
+      );
+      let json = Object.values(response.data);
+      this.setState({ words: json });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      check: true,
+    });
+  };
+
+  render() {
+    let pointsCount = 0;
+    this.state.points.forEach((point) => {
+      if (point === 1) pointsCount++;
+    });
+    pointsCount = pointsCount + "/" + this.state.words.length;
+
+    if (this.state.words.length === 0) {
+      return <p>loading...</p>;
+    } else {
+      let i = 0;
+      let ui = this.state.words.map((word) => (
+        <Word
+          key={word.id}
+          count={i++}
+          pair={word}
+          check={this.state.check}
+          handler={this.handler}
+        />
+      ));
+      return (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>English</th>
+                <th>Finnish</th>
+              </tr>
+            </thead>
+            {ui}
+          </table>
+          <p>Score: {pointsCount}</p>
+          <button onClick={this.handleClick} disabled={this.state.check}>
+            Check answers
+          </button>
+        </div>
+      );
+    }
+  }
+}
+
+export default List;
---
+++ frontend/src/routes/tags.js
@@ -0,0 +1,50 @@
+import "../App.css";
+import List from "../components/list";
+import React from "react";
+const axios = require("axios").default;
+
+class Tags extends React.Component {
+  state = { tags: [], select: 0 };
+  async componentDidMount() {
+    try {
+      const response = await axios.get("http://localhost:8080/tags");
+      let json = Object.values(response.data);
+      this.setState({ tags: json });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    if (this.state.tags.length === 0) {
+      return <p>loading...</p>;
+    } else {
+      if (this.state.select === 0) {
+        let ui = this.state.tags.map((tag) => (
+          <div key={tag.id}>
+            <button onClick={() => this.setState({ select: tag.id })}>
+              {tag.tag}
+            </button>
+          </div>
+        ));
+        return (
+          <div>
+            <h2>Select what words you want to learn</h2>
+            <ul>{ui}</ul>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <button onClick={(e) => this.setState({ select: 0 })}>
+              Go Back
+            </button>
+            <List tag={this.state.select}></List>
+          </div>
+        );
+      }
+    }
+  }
+}
+
+export default Tags;
